5316b09afe85c22240b1f2cb5c73bc8a99c216f1
SergeyFF3 <specialforce64044@gmail.com>
2022-10-22 19:24:47 +0300

    add jest, add not found page, add loader

--- src/app/providers/router/UI/AppRouter.tsx
+++ src/app/providers/router/UI/AppRouter.tsx
@@ -1,20 +1,24 @@
 import React, {Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
 import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import Loader from "widgets/PageLoader/Loader";
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({element, path}) => (
-                    <Route
+        <Routes>
+            {Object.values(routeConfig).map(({element, path}) => (
+                <Route
                     key={path}
                     path={path}
-                    element={ <div className='page-wrapper'>{element}</div> }
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+                    element={(
+                        <Suspense fallback={<Loader/>}>
+                            <div className='page-wrapper'>
+                                {element}
+                            </div>
+                        </Suspense>)}
+                />
+            ))}
+        </Routes>
     );
 };
 
---
+++ src/widgets/PageLoader/Loader.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import './Loader.scss'
+
+interface LoaderProps {
+    className?: string
+}
+
+const Loader = ({className}: LoaderProps) => {
+
+    return (
+        <div className={classNames('page__loader', {}, [className])}>
+            <div className="lds-spinner">
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+                <div />
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
